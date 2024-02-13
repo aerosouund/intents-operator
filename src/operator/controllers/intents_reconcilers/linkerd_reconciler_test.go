dbd73d983d04de9396af6042a2543bc642543bcf
aerosouund <aerosound161@gmail.com>
2024-02-13 20:43:02 +0200

    working test

--- src/operator/controllers/intents_reconcilers/linkerd_reconciler_test.go
+++ src/operator/controllers/intents_reconcilers/linkerd_reconciler_test.go
@@ -5,25 +5,25 @@ import (
 	"fmt"
 	"testing"
 
+	linkerdserver "github.com/linkerd/linkerd2/controller/gen/apis/server/v1beta1"
 	"github.com/stretchr/testify/suite"
 	"go.uber.org/mock/gomock"
-	v1 "k8s.io/api/core/v1"
-	v12 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 
-	"github.com/otterize/intents-operator/src/operator/api/v1alpha3"
 	otterizev1alpha3 "github.com/otterize/intents-operator/src/operator/api/v1alpha3"
 	mocks "github.com/otterize/intents-operator/src/operator/controllers/intents_reconcilers/mocks"
 	"github.com/otterize/intents-operator/src/shared/testbase"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 type LinkerdReconcilerTestSuite struct {
 	testbase.MocksSuiteBase
+	scheme          *runtime.Scheme
 	admin           *LinkerdReconciler
 	ldm             *mocks.MockLinkerdPolicyManager
 	serviceResolver *mocks.MockServiceResolver
@@ -31,7 +31,15 @@ type LinkerdReconcilerTestSuite struct {
 
 func (s *LinkerdReconcilerTestSuite) SetupTest() {
 	s.MocksSuiteBase.SetupTest()
-	s.admin = NewLinkerdReconciler(s.Client, &runtime.Scheme{}, []string{}, true, true)
+	s.serviceResolver = mocks.NewMockServiceResolver(s.Controller)
+	s.scheme = runtime.NewScheme()
+	s.scheme.AddKnownTypeWithName(schema.GroupVersionKind{Group: "policy.linkerd.io", Version: "v1beta1", Kind: "servers"}, &linkerdserver.Server{})
+	s.admin = NewLinkerdReconciler(s.Client, s.scheme, []string{}, true, true)
+	s.ldm = mocks.NewMockLinkerdPolicyManager(s.Controller)
+
+	s.admin.Recorder = s.Recorder
+	s.admin.serviceIdResolver = s.serviceResolver
+	s.admin.linkerdManager = s.ldm
 }
 
 func (s *LinkerdReconcilerTestSuite) TearDownTest() {
@@ -70,6 +78,8 @@ func (s *LinkerdReconcilerTestSuite) TestCreatePolicy() {
 		Spec: intentsSpec,
 	}
 
+	s.expectValidatingLinkerdInstalled()
+
 	// Initial call to get the ClientIntents object when reconciler starts
 	emptyIntents := &otterizev1alpha3.ClientIntents{}
 	s.Client.EXPECT().Get(gomock.Any(), req.NamespacedName, gomock.Eq(emptyIntents)).DoAndReturn(
@@ -84,6 +94,9 @@ func (s *LinkerdReconcilerTestSuite) TestCreatePolicy() {
 	clientServiceAccount := "test-server-sa"
 	clientPod := v1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				"linkerd.io/inject": "enabled",
+			},
 			Name:      "test-client-fdae32",
 			Namespace: serverNamespace,
 		},
@@ -100,72 +113,15 @@ func (s *LinkerdReconcilerTestSuite) TestCreatePolicy() {
 		},
 	}
 
-	serverPod := v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-server-2b5e0d",
-			Namespace: serverNamespace,
-		},
-		Spec: v1.PodSpec{
-			ServiceAccountName: "test-server-sa",
-			Containers: []v1.Container{
-				{
-					Name: "server-who-listens",
-				},
-				{
-					Name: "linkerd-proxy",
-				},
-			},
-		},
-	}
 	s.serviceResolver.EXPECT().ResolveClientIntentToPod(gomock.Any(), gomock.Eq(intentsObj)).Return(clientPod, nil)
-	s.serviceResolver.EXPECT().ResolveIntentServerToPod(gomock.Any(), gomock.Eq(intentsObj.Spec.Calls[0]), serverNamespace).Return(serverPod, nil)
 	s.ldm.EXPECT().Create(gomock.Any(), gomock.Eq(&intentsObj), clientServiceAccount).Return(nil)
 	res, err := s.admin.Reconcile(context.Background(), req)
 	s.NoError(err)
 	s.Empty(res)
 }
 
-func (s *LinkerdReconcilerTestSuite) TestAnything() {
-	_ = &v1alpha3.ClientIntents{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test",
-			Namespace: "test",
-		},
-		Spec: &v1alpha3.IntentsSpec{
-			Service: v1alpha3.Service{
-				Name: clientName,
-			},
-			Calls: []v1alpha3.Intent{
-				{
-					Name: "test",
-				},
-			},
-		},
-	}
-
-	// This object will be returned to the reconciler's Get call when it calls for a CRD named "servers.policy.linkerd.io"
-	crd := &v12.CustomResourceDefinition{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "servers.policy.linkerd.io",
-		},
-	}
-
-	// The matchers here make it check for a CRD called "servers.policy.linkerd.io"
-	s.Client.EXPECT().Get(gomock.Any(), types.NamespacedName{Name: "servers.policy.linkerd.io"}, gomock.AssignableToTypeOf(crd)).Do(
-		func(ctx context.Context, key types.NamespacedName, obj *v12.CustomResourceDefinition, opts ...metav1.GetOptions) error {
-			// Copy the struct into the target pointer struct
-			*obj = *crd
-			return nil
-		})
-
-	res, err := s.admin.Reconcile(context.Background(), reconcile.Request{
-		NamespacedName: types.NamespacedName{
-			Name:      "test",
-			Namespace: "test",
-		},
-	})
-	s.Require().NoError(err)
-	s.Require().Empty(res)
+func (s *LinkerdReconcilerTestSuite) expectValidatingLinkerdInstalled() {
+	s.Client.EXPECT().Get(gomock.Any(), types.NamespacedName{Name: "servers.policy.linkerd.io"}, gomock.Any()).Return(nil)
 }
 
 func TestLinkerdReconcilerSuite(t *testing.T) {
